be5a2059492ed92ea578a06dba6f53f40b2a422d
michelmiller <martsaudiovisual@gmail.com>
2022-12-03 15:05:37 -0300

    icons route

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,5 +1,9 @@
+
+
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Feather from 'react-native-vector-icons/Feather';
 
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
@@ -11,10 +15,49 @@ const Tab = createBottomTabNavigator();
 
 function AppRoutes() {
  return (
-   <Tab.Navigator >
-       <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-       <Tab.Screen name="Search" component={Search} options={{ headerShown: false }} />
-       <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+   <Tab.Navigator
+   tabBarOptions={{
+     keyboardHidesTabBar: true,
+     showLabel: false,
+     style:{
+       backgroundColor: '#202225',
+       borderTopWidth: 0,
+     },
+
+     activeTintColor: '#FFF'
+   }}
+   >
+       <Tab.Screen 
+       name="Home" 
+       component={Home} 
+       options={{ headerShown: false,
+         tabBarIcon: ({ color, size })  => {
+       
+          return <Feather name="home" color={color} size={size} />
+         }
+       }}
+       />
+
+       <Tab.Screen 
+       name="Search" 
+       component={Search} 
+       options={{ headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+          return <Feather name="search" color={color} size={size} />
+          }
+        }}       
+       />
+
+       <Tab.Screen 
+       name="Profile" 
+       component={Profile} 
+       options={{ headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            
+          return <Feather name="user" color={color} size={size} />
+          }
+        }}       
+       />
    </Tab.Navigator>
   );
 }
